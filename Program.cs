ea72056e721bfdff6fe10a374006134c14c424f9
NehalDutta-7002089125 <nihal326dutta@gmail.com>
2022-12-01 22:06:53 +0530

    Nehal

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Fabonacchi
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           
+            int a = 0, b = 1, c;
+           Console.WriteLine(a);
+            Console.WriteLine(b);
+
+               for (int i = 0; i < 10; i++)
+               {
+                c = a + b;
+                a++;
+                b++;
+             
+                Console.WriteLine(c);
+               }
+        }
+    }
+}
