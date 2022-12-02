baa051b8efab138c431f2db3457b22466f0d33a9
NehalDutta-7002089125 <nihal326dutta@gmail.com>
2022-12-02 18:49:00 +0530

    Nehal

--- Program.cs
+++ Program.cs
@@ -1,24 +1,30 @@
 ﻿using System;
 
-namespace TempConversion
+namespace MonthlyPayment
 {
     internal class Program
     {
-        public static void temp()
+        static void Main(string[] args)
         {
-            
-            int C = 6;
-            int F = 8;
-            
-                F = (C * 9 / 5) + 32;
-                Console.WriteLine("F:" + F);
+            double  P, Y, R, n, r, Payment;
 
-            int c=6, f=8;
-                c = (f + 1);
-                
-                Console.WriteLine("c:" + c);
-                
-            
+            Console.WriteLine("Enter Loan Amount : ");
+
+            P = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("No. of years to pay the principle amount : ");
+
+            Y = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Enter rate of interest : ");
+
+            R = Convert.ToDouble(Console.ReadLine());
+
+            n = 12 * Y; r = R / (12 * 100);
+
+            Payment = (P * r) / (1 - Math.Pow(1 + r, -n));
+
+            Console.WriteLine(Payment);
 
         }
     }
