837e4b7e60d0543a3156f34644b14c03a03accf7
NehalDutta-7002089125 <118413398+NehalDutta-7002089125@users.noreply.github.com>
2022-12-02 17:58:02 +0530

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -7,13 +7,13 @@ namespace TempConversion
         public static void temp()
         {
             
-            int C = 6;
-            int F = 8;
+            double C = 6;
+            double F = 8;
             
                 F = (C * 9 / 5) + 32;
                 Console.WriteLine("F:" + F);
 
-            int c=6, f=8;
+            doble c=6, f=8;
                 c = (f + 1);
                 
                 Console.WriteLine("c:" + c);
