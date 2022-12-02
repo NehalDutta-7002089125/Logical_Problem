36d281b2f28e4d0f73662c8133abf1a5c70226e5
NehalDutta-7002089125 <nihal326dutta@gmail.com>
2022-12-02 20:49:11 +0530

    Nehal

--- Program.cs
+++ Program.cs
@@ -1,25 +1,62 @@
 ﻿using System;
+using System.Globalization;
 
-namespace squareroot
+namespace Dayoffweek
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-               Console.WriteLine("Enter a number : ");
+            int MonthNum= 0, year, d, y, x, m, day;
 
-            double c = Convert.ToDouble(Console.ReadLine());
-            
-            double t = c;  
-            
-            double EPSILON = 1e-15;
+            Console.WriteLine("Enter Year : ");
 
-            while (Math.Abs(t * t - c) > EPSILON) 
+            year = Convert.ToInt32(Console.ReadLine());
 
+            Console.WriteLine("Enter Month : ");
+
+            string monthName = Console.ReadLine();
+
+            Console.WriteLine("Enter Date : ");
+
+            d = Convert.ToInt32(Console.ReadLine());
+
+            y = year - (14 - MonthNum) / 12;
+
+            x = y + y / 4 - y / 100 + y / 400;
+
+            m = MonthNum + 12 * ((14 - MonthNum) / 12) - 2;
+
+            day = (d + x + (31 * m) / 12) % 7;
+
+            Console.WriteLine("Day number is : " + day);
+
+            switch (day)
             {
-                t = (c / t + t) / 2.0; 
+                case 0:
+                    Console.WriteLine("Sunday");
+                    break;
+                case 1:
+                    Console.WriteLine("Monday");
+                    break;
+                case 2:
+                    Console.WriteLine("Tuesday");
+                    break;
+                case 3:
+                    Console.WriteLine("Wednesday");
+                    break;
+                case 4:
+                    Console.WriteLine("Thursday");
+                    break;
+                case 5:
+                    Console.WriteLine("Friday");
+                    break;
+                case 6:
+                    Console.WriteLine("Saturday");
+                    break;
+                default:
+                    break;
             }
-            Console.WriteLine("The square root of c is :"+t);
         }
     }
 }
