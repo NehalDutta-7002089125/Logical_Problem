19ba89caddcdfd5ad32b2b50524560b7c44b4162
NehalDutta-7002089125 <118413398+NehalDutta-7002089125@users.noreply.github.com>
2022-12-04 14:27:29 +0530

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -12,10 +12,8 @@ namespace PrimeNo
 
             a = int.Parse(Console.ReadLine());
 
-            for (i =2; i <a / 2; i++)
-
-            {
-                if (a % i == 0)
+            
+              if (a % 2 == 0)
 
                 {
                     Console.Write(a + "is not a Prime number.");
@@ -28,7 +26,7 @@ namespace PrimeNo
 
                     Console.Write(a + " is a Prime number.");
                 }
-            }
+            
         }
     }
 }
